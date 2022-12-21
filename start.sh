c4b419d22f24a96052feb2bdc265c9ec50835d23
Moviehouse12 <119985625+Moviehouse12@users.noreply.github.com>
2022-12-21 20:06:59 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Cyniteofficial/MdiskSearchBotV2.git /MdiskSearchBotV2
-cd /MdiskSearchBotV2
+git clone https://github.com/pushkar_dhiwar/MdiskSearch2Bot.git /MdiskSearch2Bot
+cd /MdiskSearch2Bot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
